6c20a1e6b33ec15c0b03750d8d6440733087a47d
salmankmohd <salmandev007@gmail.com>
2024-11-26 15:48:29 +0530

    just commit

---
+++ SparkSupportMachineTest/ViewControllers/WeatherData.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherData.swift
+//  SparkSupportMachineTest
+//
+//  Created by Salman Macbook Air M3 on 26/11/24.
+//
+
+import Foundation
---
+++ SparkSupportMachineTest/ViewControllers/WeatherService.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherService.swift
+//  SparkSupportMachineTest
+//
+//  Created by Salman Macbook Air M3 on 26/11/24.
+//
+
+import Foundation
